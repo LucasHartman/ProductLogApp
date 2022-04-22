7ebde268ffe7517bad929defc3cc73d830ff19fe
LucasHartman <ljh.hartman@gmail.com>
2022-04-22 17:21:21 +0200

    project setup

---
+++ src/main/java/com/pack/productlog/ProductlogApplication.java
@@ -0,0 +1,13 @@
+package com.pack.productlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pack/productlog/ProductlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.productlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
